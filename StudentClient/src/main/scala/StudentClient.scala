7bbbc1b425b6c0930632d99a3d54dde213d7dbe9
Vadym Opryshko <vo@underdefense.com>
2020-10-29 12:01:23 +0200

    Add addStudents method

--- StudentClient/src/main/scala/StudentClient.scala
+++ StudentClient/src/main/scala/StudentClient.scala
@@ -1,8 +1,11 @@
+import akka.actor.Status.{Failure, Success}
 import akka.actor.{ActorSystem, ClassicActorSystemProvider}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.stream.{ActorMaterializer, Materializer}
+import akka.http.scaladsl.model.headers.RawHeader
+import akka.stream.Materializer
 import akka.http.scaladsl.unmarshalling.{Unmarshal, Unmarshaller}
+import spray.json._
 
 import scala.concurrent.ExecutionContext
 // for JSON serialization/deserialization following dependency is required:
@@ -10,13 +13,11 @@ import scala.concurrent.ExecutionContext
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
-import scala.concurrent.{Await, ExecutionContextExecutor, Future}
-import scala.util.{Failure, Success}
-import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.io.StdIn.readLine
 
 final case class Student(name: String, age: Int, mark: Double)
-
+final case class StudentRecords(records: List[Student])
 
 object StudentClient extends App{
 
@@ -28,24 +29,26 @@ object StudentClient extends App{
   while (true) {
     val choice = journal.menu
     choice match {
-      //      case 1 =>
-      //        println("Student added")
-      //        journal.addStudent()
       case 1 =>
+        println("Student added")
+//        journal.addStudent()
+      case 2 =>
         println("Show students\n")
         journal.showStudents()
-      //      case 3 =>
-      //        println("Delete a student\n")
-      //        journal.deleteStudent()
+      case 3 =>
+        println("Delete a student\n")
+//        journal.deleteStudent()
       case 2 =>
         println("Exit App")
         System.exit(0)
+      case _ =>
     }
   }
 }
 
 class RestClient(implicit ec: ExecutionContext, mat: Materializer, system: ClassicActorSystemProvider) {
   implicit val studentFormat: RootJsonFormat[Student] = jsonFormat3(Student)
+  implicit val journalFormat: RootJsonFormat[StudentRecords] = jsonFormat1(StudentRecords)
   private val baseUrl = "http://localhost:8080/student"
 
   def fetchStudents: Future[List[Student]] = {
@@ -59,8 +62,28 @@ class RestClient(implicit ec: ExecutionContext, mat: Materializer, system: Class
     }
   }
 
+    def addStudents(student: List[Student]): Unit = {
+      val records = StudentRecords(student)
 
+      val responseFuture: Future[HttpResponse] =
+        Http(system).singleRequest(
+          HttpRequest(
+            method = HttpMethods.POST,
+            uri = baseUrl,
+            entity = HttpEntity(ContentTypes.`application/json`, records.toJson)
+          )
+        )
 
+//      val searchObject = net.liftweb.json.Serialization.write(records) //req is search object
+//      val searchObjectEntity = HttpEntity(ContentTypes.`application/json`, searchObject)
+//      val request = HttpRequest(HttpMethods.POST, baseUrl, searchObjectEntity)
+
+      responseFuture
+        .onComplete {
+          case Success(res) => println(res)
+          case Failure(_)   => sys.error("something wrong")
+        }
+    }
 }
 
 class StudentJournal(implicit ec: ExecutionContext, mat: Materializer, system: ClassicActorSystemProvider) {
@@ -68,17 +91,20 @@ class StudentJournal(implicit ec: ExecutionContext, mat: Materializer, system: C
   val client = new RestClient
   var cache: Map[String, List[Student]] = Map.empty
 
-  //  def addStudent(): Unit = {
-  //    println("Enter Student Name")
-  //    val name: String = readLine()
-  //    println("Enter Student Age")
-  //    val age: Int = readLine().toInt
-  //    println("Enter Student average mark")
-  //    val mark: Double = readLine().toDouble
-  //
-  //    val student = Student(name, age, mark)
-  //
-  //  }
+    def addStudent(): Unit = {
+      println("Enter Student Name")
+      val name: String = readLine()
+      println("Enter Student Age")
+      val age: Int = readLine().toInt
+      println("Enter Student average mark")
+      val mark: Double = readLine().toDouble
+
+      val student = Student(name, age, mark)
+
+      client.addStudents(List(student))
+
+    }
+
 
   def showStudents():Unit = {
 
@@ -88,87 +114,57 @@ class StudentJournal(implicit ec: ExecutionContext, mat: Materializer, system: C
       printList(cache.values.flatten.toList)
     }
 
+//    if(cache.isEmpty) {
+//      client.fetchStudents.map {
+//        students => cache("Students") = students
+//      }
+//      printList(cache.values.flatten.toList)
+//    } else {
+//      printList(cache.values.flatten.toList)
+//    }
+//
+//
+//    if(cache.isEmpty) {
+//      client.fetchStudents.map {
+//        students => cache += ("Students" -> students)
+//      }
+//      printList(cache.values.flatten.toList)
+//    } else {
+//      printList(cache.values.flatten.toList)
+//    }
 
-    //    print("You can sort the list by:\n" +
-    //      "1 - Sort students list by name\n" +
-    //      "2 - Sort students list by age\n" +
-    //      "3 - Sort student list by average mark\n" +
-    //      "4 - Go to main menu\n")
-    //    print("chose an action: ")
-    //    val action: Int = readLine().toInt
-    //    action match {
-    //      case 1 => sort("name")
-    //      case 2 => sort("age")
-    //      case 3 => sort("mark")
-    //      case 4 =>
-    //      case _ => println("Please chose an action")
-    //    }
-  }
+    val students = cache.values.flatten.toList
+    Thread.sleep(2000)
+    print("You can sort the list by:\n" +
+      "1 - Sort students list by name\n" +
+      "2 - Sort students list by age\n" +
+      "3 - Sort student list by average mark\n" +
+      "4 - Go to main menu\n")
+    print("chose an action: ")
+    val action: Int = readLine().toInt
+    action match {
+      case 1 =>
+        val sorted = students.sortWith(_.name < _.name)
+        printList(sorted)
+      case 2 =>
+        printList(students.sortBy(_.age))
+      case 3 =>2
+        printList(students.sortBy(_.age))
+      case 4 =>
+      case _ => println("Please chose an action")
+    }
+ }
 
-  //  def sort(mode: String, students: List[Student]): Unit = {
-  //
-  //    var swapped: Boolean = true
-  //    if (mode == "age") {
-  //      while (swapped) {
-  //        swapped = false
-  //        for (i <- 0 until students.length - 1) {
-  //          if (students(i).age > students(i + 1).age) {
-  //            val temp = students(i)
-  //            students(i) = students(i + 1)
-  //            students(i + 1) = temp
-  //            swapped = true
-  //          }
-  //        }
-  //      }
-  //      printList(students)
-  //    } else if (mode == "mark") {
-  //      while (swapped) {
-  //        swapped = false
-  //        for (i <- 0 until studList.length - 1) {
-  //          if (studList(i).mark > studList(i + 1).mark) {
-  //            val temp = studList(i)
-  //            studList(i) = studList(i + 1)
-  //            studList(i + 1) = temp
-  //            swapped = true
-  //          }
-  //        }
-  //      }
-  //      printList(studList)
-  //    } else if (mode == "name") {
-  //      val sorted = studList.sortWith(_.name < _.name)
-  //      printList(sorted)
-  //    }
-  //  }
-
-  //  def deleteStudent(): Unit = {
-  //    val studList = loadFile(fileName)
-  //    printList(studList)
-  //    print("You can delete student:\n" +
-  //      "1 - delete by name\n" +
-  //      "2 - delete by ordering number\n" +
-  //      "3 - Go to main menu\n")
-  //    print("Chose an action: ")
-  //    val clone = studList.clone()
-  //    val action: Int = readLine().toInt
-  //    action match {
-  //      case 1 =>
-  //        print("Type name: ")
-  //        val name: String = readLine()
-  //        for (std <- clone) {
-  //          if (std.name.equals(name)) {
-  //            studList.remove(studList.indexOf(std))
-  //          }
-  //        }
-  //      case 2 =>
-  //        println("Type ordering number")
-  //        val num: Int = readLine().toInt
-  //        studList.remove(num)
-  //      case 3 =>
-  //      case _ =>
-  //        println("Please chose an action")
-  //    }
-  //    printList(studList)
-  //  }
+//  def deleteStudent(): Unit = {
+//
+//    printList(studList)
+//    print("You can delete student:\n" +
+//      "1 - delete by name\n" +
+//      "2 - delete by ordering number\n" +
+//      "3 - Go to main menu\n")
+//    print("Chose an action: ")
+//    val clone = studList.clone()
+//    val action: Int = readLine().toInt
 
 
   def printList(list: List[Student]): Unit = {
@@ -182,9 +178,9 @@ class StudentJournal(implicit ec: ExecutionContext, mat: Materializer, system: C
     var choice = 0
     println("Student Client")
     try {
-      //      println("1 - Add a student")
-      println("1 - Show students")
-      //      println("3 - Delete a student")
+      println("1 - Add a student")
+      println("2 - Show students")
+      println("3 - Delete a student")
       println("4 - Exit the App")
       choice = readLine().toInt
     } catch {
@@ -193,9 +189,3 @@ class StudentJournal(implicit ec: ExecutionContext, mat: Materializer, system: C
     choice
   }
 }
-
-// UserRest -> Validation
-//   UserService/UserManager -> UserDTO
-//     UserDao/UserRepository -> Errors handling
-//       fetch from database
-// SOLID
